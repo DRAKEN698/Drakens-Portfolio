583abd849049cba23584961e547745af613ae3f5
DRAKEN698 <ruturajwalke9@gmail.com>
2026-09-15 02:20:38 +0530

    draken

--- script.js
+++ script.js
@@ -135,7 +135,9 @@ document.addEventListener("DOMContentLoaded", () => {
 // =========================================
 
 gsap.registerPlugin(ScrollTrigger);
-
+ScrollTrigger.config({ 
+    ignoreMobileResize: true 
+});
 // Initial States set karna (Hidden states)
 gsap.set(".header-group", { y: 50, opacity: 0 });
 gsap.set(".intro-text", { y: 30, opacity: 0 });
